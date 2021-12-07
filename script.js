e83a6e00833d7ee83252027e755e1c5b5375661a
Vinizin05 <89863696+Vinizin05@users.noreply.github.com>
2021-12-07 00:01:32 -0300

    finalizando

---
+++ script.js
@@ -0,0 +1,33 @@
+const turnOn = document.getElementById("turnOn");
+const turnOff = document.getElementById("turnOff");
+const lamp = document.getElementById("lamp");
+
+turnOn.addEventListener("click", lampOn);
+
+turnOff.addEventListener("click", lampOff);
+
+lamp.addEventListener("mouseover", lampOn);
+
+lamp.addEventListener("mouseleave", lampOff);
+
+lamp.addEventListener("dblclick", lampBroken);
+
+function islampbroken() {
+  return lamp.src.indexOff("quebrada") > -1;
+}
+
+function lampOn() {
+  if (!islampbroken()) {
+    lamp.src = "./img/ligada.jpg";
+  }
+}
+
+function lampOff() {
+  if (!islampbroken()) {
+    lamp.src = "./img/desligada.jpg";
+  }
+}
+
+function lampBroken() {
+  lamp.src = "./img/quebrada.jpg";
+}
